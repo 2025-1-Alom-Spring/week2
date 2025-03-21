c8c7fc9f2fca8cdca4c603ba08d6f75ecbbf9cfa
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-03-21 22:19:52 +0900

    update : Add handlerCustomException

--- src/main/java/com/example/week2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/week2/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.week2.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
