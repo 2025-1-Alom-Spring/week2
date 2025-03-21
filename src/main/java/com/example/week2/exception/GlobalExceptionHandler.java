51aa959a8e45e6321536e1d61736f41ac9d5f23e
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-03-21 22:00:41 +0900

    update : Add handlerCustomException

--- src/main/java/com/example/week2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/week2/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.week2.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +20,16 @@ public class GlobalExceptionHandler {
   public String handleInternalError() {
     log.error("InternalError 처리 시작");
     return "InternalError 핸들링";
+  }
+
+  @ExceptionHandler(CustomException.class)
+  public ResponseEntity<ErrorResponse> handlerCustomException(CustomException e) {
+    log.error("CustomException 발생: {}", e.getMessage(), e);
+    ErrorCode errorCode = e.getErrorCode();
+    ErrorResponse response = ErrorResponse.builder()
+            .errorCode(errorCode)
+            .errorMessage(errorCode.getMessage())
+            .build();
+    return ResponseEntity.status(errorCode.getStatus()).body(response);
   }
 }
