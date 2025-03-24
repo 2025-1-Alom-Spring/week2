2a78c6c5117bc4220928743bc887b19abb7f7cc2
Subin Choi <125132754+subi930@users.noreply.github.com>
2025-03-24 13:55:13 +0900

    update : 2주차 과제

--- src/main/java/com/example/week2/builder/App1.java
+++ src/main/java/com/example/week2/builder/App1.java
@@ -1,8 +1,22 @@
 package com.example.week2.builder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App1 {
 
+  private static final Logger log = LoggerFactory.getLogger(App1.class);
+
   public static void main(String[] args) {
+
+    log.error("에러입니다.");
+    Student student = Student.builder()
+            .name("최수빈")
+            .age(25)
+            .school("세종대학교")
+            .build();
+
+    System.out.println(student);
 
   }
 }
--- src/main/java/com/example/week2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/week2/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.week2.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(NullPointerException.class)
+  @ExceptionHandler(NullPointerException.class) // null포인터익셉션을 잡아냄
   public String handleNullPointerException() {
+    // 잡아낸 후 다음 코드 실행
     log.error("NullPointer Exception 처리 시작");
     return "NullPointer Exception 핸들링";
   }
@@ -18,5 +20,19 @@ public class GlobalExceptionHandler {
   public String handleInternalError() {
     log.error("InternalError 처리 시작");
     return "InternalError 핸들링";
+  }
+
+  @ExceptionHandler(CustomException.class)
+  public ResponseEntity<ErrorResponse> handleCustomException(CustomException e){
+    log.error("CustomException 발생: {}", e.getMessage(), e);
+
+    ErrorCode errorCode = e.getErrorCode();
+
+    ErrorResponse response = ErrorResponse.builder()
+            .errorCode(errorCode)
+            .errorMessage(errorCode.getMessage())
+            .build();
+
+    return ResponseEntity.status(errorCode.getStatus()).body(response);
   }
 }
