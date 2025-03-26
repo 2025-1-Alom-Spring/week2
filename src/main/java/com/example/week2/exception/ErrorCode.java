9c190d26fc08e4e5dc59a1ec2966cc3b38ebd029
daye200 <daye200@naver.com>
2025-03-26 17:23:04 +0900

    feat(exception):예외처리

--- src/main/java/com/example/week2/exception/ErrorCode.java
+++ src/main/java/com/example/week2/exception/ErrorCode.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
-  INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청이 들어왔습니다"),;
+//http 상태코드, 메세지 받음
+  INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청이 들어왔습니다"),
+  SEJONG_UNI(HttpStatus.BAD_GATEWAY,"세종대왕이 아닙니다");
 
 
   private final HttpStatus status;
--- src/main/java/com/example/week2/exception/ExceptionController.java
+++ src/main/java/com/example/week2/exception/ExceptionController.java
@@ -21,4 +21,9 @@ public class ExceptionController {
     throw new IllegalArgumentException();
   }
 
+  @GetMapping("/custom")
+  public void throwCustomException(){
+    throw new CustomException(ErrorCode.SEJONG_UNI);
+
+  }
 }
