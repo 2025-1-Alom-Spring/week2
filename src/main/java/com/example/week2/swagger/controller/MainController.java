56f3eb84a86fef86f859af60e42afba21c92a61c
jihwanyeom <yum020428@gmail.com>
2025-03-26 23:12:07 +0900

    feat : 과제 2 커밋

--- src/main/java/com/example/week2/swagger/controller/MainController.java
+++ src/main/java/com/example/week2/swagger/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
   @GetMapping("/main")
-  public String mainMethod(@RequestParam String str) {
+  public String mainMethod(@RequestParam("Error") String str) {
     return str;
   }
 
